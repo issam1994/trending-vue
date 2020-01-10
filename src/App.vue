69d753fb550141727775425fd80bd0717d866a35
issam1994 <issamaitouahmane@gmail.com>
2020-01-10 16:45:27 +0100

    installed tailwindcss & finished an example component that will represent a row in the list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="bg-gray-200 py-8">
+  <div class="container mx-auto">
+    <RepoRow />
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RepoRow from '@/components/RepoRow.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    RepoRow
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/RepoRow.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- repo row -->
+  <div class="flex items-center px-2 py-4 rounded-lg shadow bg-white mb-4">
+    <img class="w-32 h-32 mx-4 rounded-full object-cover border" src="https://static.javatpoint.com/tutorial/tensorflow/images/tensorflow-tutorial.png" alt="avatar" />
+    <div class="px-2">
+      <div class="text-4xl font-bold tracking-wide">Tensorflow</div>
+      <div class="font-medium pb-2">An Open Source Machine Learning framwork..</div>
+      <div class="flex items-center">
+        <div class="flex-none shadow-sm bg-gray-100 font-semibold px-2 py-1 mr-2">
+          Stars:
+          <span class="font-normal">16.9k</span>
+        </div>
+        <div class="flex-none shadow-sm bg-gray-100 font-semibold px-2 py-1 mr-2">
+          Issues:
+          <span class="font-normal">1.7k</span>
+        </div>
+        <div class="text-gray-500">
+          <span>Submitted </span>
+          <span>30 days ago </span>
+          <span>by tensorflow </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+</style>
