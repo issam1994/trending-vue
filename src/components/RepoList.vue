5f26491efe18c4b75614dcb7930879cdbe9c3dc6
issam1994 <issamaitouahmane@gmail.com>
2020-01-17 15:29:50 +0100

    refactoring data fetching functionality to use axios instead & async await with try catch blocks instead of using a promise & fetch api

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -10,23 +10,25 @@
 import {
   hasScrolledToTheEndOfList,
   thirtyDaysBeforeToday
-} from '@/components/utils.js'
+} from "@/components/utils.js";
 //importing the repo component
-import RepoRow from '@/components/RepoRow'
-import MySpinner from '@/components/MySpinner'
-//
+import RepoRow from "@/components/RepoRow";
+import MySpinner from "@/components/MySpinner";
+// importing axios
+import axios from "axios";
+
 export default {
-  name: 'RepoList',
+  name: "RepoList",
   components: {
     RepoRow,
     MySpinner
   },
   // lifesycle methods
   mounted() {
-    this.getDataFromApi()
+    this.getDataFromApi();
   },
   destroyed() {
-    this.stopListeningToScrolling()
+    this.stopListeningToScrolling();
   },
   // state
   data() {
@@ -34,57 +36,61 @@ export default {
       repos: [],
       isLoading: true,
       currentPage: 0
-    }
+    };
   },
   // methods
   methods: {
     //spinner control
-    toggleSpinner(){
+    toggleSpinner() {
       this.isLoading = !this.isLoading;
     },
     // data controls
-    getDataFromApi() {
-      console.log('getting repos..')
-      let { currentPage } = this
-      let apiUrl = `https://api.github.com/search/repositories?q=created:>${thirtyDaysBeforeToday()}&sort=stars&order=desc${'&page=' +
-        (currentPage + 1)}`
-      fetch(apiUrl)
-        .then(res => res.json())
-        .then(this.extractReposFromFetchedData)
+    async getDataFromApi() {
+      try {
+        console.log("getting repos..");
+        let { currentPage } = this;
+        let apiUrl = `https://api.github.com/search/repositories?q=created:>${thirtyDaysBeforeToday()}&sort=stars&order=desc${"&page=" +
+          (currentPage + 1)}`;
+        let data = (await axios.get(apiUrl)).data;
+        return this.extractReposFromFetchedData(data);
+      } catch (e) {
+        //retry fetching data
+        return this.getDataFromApi();
+      }
     },
     // ..
     loadNextPage() {
       if (hasScrolledToTheEndOfList()) {
         this.stopListeningToScrolling();
         this.toggleSpinner();
-        this.getDataFromApi()
+        this.getDataFromApi();
       }
     },
     // ..
     extractReposFromFetchedData(data) {
-      console.log('extracting repos..')
-      let { items: newRepos } = data
-      this.updateRepos(newRepos)
-      this.listenToScrolling()
+      console.log("extracting repos..");
+      let { items: newRepos } = data;
+      this.updateRepos(newRepos);
+      this.listenToScrolling();
     },
     // ..
     updateRepos(fetchedRepos) {
-      console.log('updating repos..')
-      let { repos, currentPage } = this
-      this.repos = [...repos, ...fetchedRepos]
+      console.log("updating repos..");
+      let { repos, currentPage } = this;
+      this.repos = [...repos, ...fetchedRepos];
       this.toggleSpinner();
-      this.currentPage = currentPage + 1
+      this.currentPage = currentPage + 1;
     },
     //scroll controls
     listenToScrolling() {
-      return window.addEventListener('scroll', this.loadNextPage)
+      return window.addEventListener("scroll", this.loadNextPage);
     },
     // ..
     stopListeningToScrolling() {
-      return window.removeEventListener('scroll', this.loadNextPage)
+      return window.removeEventListener("scroll", this.loadNextPage);
     }
   }
-}
+};
 </script>
 
 <style>
